db36d6e786e367c541ab94bc6930559a06f14044
Sarika-T <96774688+Sarika-T@users.noreply.github.com>
2024-06-30 23:53:04 +0530

    Add files via upload

---
+++ formProject/my-app/src/component/Message/Message.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Blocker from './Blocker';
+
+const Message = () => {
+  Blocker('Are you sure you want to leave?');
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Message;
+
+
